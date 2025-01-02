378767c95e1bbcb6b64fd32d212816ba8dcea517
Nazakat-Ejazz <nazakatejaz.work@gmail.com>
2025-01-02 17:14:47 +0500

    Binded route with controller for creating new user action

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,6 +4,7 @@ import { createUser } from '../service/user.service';
 export async function createUserHandler(req: Request, res: Response) {
   try {
     const user = await createUser(req.body);
+    return user;
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);
